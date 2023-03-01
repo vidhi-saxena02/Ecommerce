ae453604aa3b133c1b94a1049469a3952153c06c
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2023-03-01 18:51:05 +0530

    product description route added

--- Backend/controller/product.controller.js
+++ Backend/controller/product.controller.js
@@ -15,6 +15,24 @@ exports.getAllProducts = async (req, res) => {
   res.status(200).json({ success: true, products });
 };
 
+//Get single Product/product details
+
+exports.getProductDetails = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
 //Admin
 exports.updateProducts = async (req, res, next) => {
   let product = await Product.findById(req.params.id);
--- Backend/routes/products.route.js
+++ Backend/routes/products.route.js
@@ -3,6 +3,7 @@ const {
   getAllProducts,
   updateProducts,
   deleteProducts,
+  getProductDetails,
 } = require("../controller/product.controller");
 
 const express = require("express");
@@ -12,6 +13,9 @@ const productRouter = express.Router();
 productRouter.route("/products").get(getAllProducts);
 productRouter.route("/products/new").post(createProduct);
 productRouter.route("/products/:id").put(updateProducts);
-productRouter.route("/products/:id").delete(deleteProducts);
+productRouter
+  .route("/products/:id")
+  .delete(deleteProducts)
+  .get(getProductDetails);
 
 module.exports = productRouter;
