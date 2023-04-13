6c521053d2308845ea096e7e0424e4ad42f2ad9b
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2023-04-14 00:25:29 +0530

    minor change

--- Backend/models/user.schema.js
+++ Backend/models/user.schema.js
@@ -37,6 +37,10 @@ const UserSchema = new mongoose.Schema({
     type: String,
     default: "user",
   },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
   resetPasswordToken: String,
   resetPasswordExpire: Date,
 });
